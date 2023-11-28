3c60ca9a079bbcef343ed527cde982382a9d43c4
Brian <chumobrian@gmail.com>
2023-11-28 16:46:53 +0300

    feat: Add a function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,13 +7,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	parent = malloc(sizeof(binary_tree_t));
-	if (parent == NULL)
+	binary_tree_t *new_node = parent;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	parent->n = value;
-	parent->left = NULL;
-	parent->right = NULL;
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-	return (parent);
+	return (new_node);
 }
