26d4cb9933956410c58c201043d0469824929813
Brian <chumobrian@gmail.com>
2023-12-03 20:37:30 +0300

    feat: Add a  function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	binary_tree_t *parent = NULL;
 	binary_tree_t *second_parent = NULL, *first_parent = NULL;
 
+	if (first->n == second->n)
+		return ((binary_tree_t *)first);
 	if (!first || !second || !first->parent || !second->parent)
 		return (NULL);
 	if (second->parent->n == first->n)
@@ -21,8 +23,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		return (first->parent);
 	first_parent = binary_trees_ancestor(first, second->parent);
 	second_parent = binary_trees_ancestor(first->parent, second);
-	if (first->n == second->n)
-		parent = (binary_tree_t *)first;
 	if ((!first_parent && second_parent) || (first_parent && second_parent))
 		parent = second_parent;
 	if (first_parent && !second_parent)
